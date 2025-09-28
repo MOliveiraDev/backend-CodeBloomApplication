30637d869ee6ab4488e3fdb249d5accc63b1b8b6
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-09-28 00:11:23 -0300

    refactor: Application initialize with dotenv variables

--- src/main/java/com/code/bloom/BloomApplication.java
+++ src/main/java/com/code/bloom/BloomApplication.java
@@ -1,12 +1,16 @@
 package com.code.bloom;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BloomApplication {
 
+
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(BloomApplication.class, args);
 	}
 
