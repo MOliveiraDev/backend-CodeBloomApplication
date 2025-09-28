f43f8a19aa685adc32fee01f129a3b9771d4d305
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-09-28 00:59:31 -0300

    Feat: create UserEntity

---
+++ src/main/java/com/code/bloom/database/entity/users/UserEntity.java
@@ -0,0 +1,45 @@
+package com.code.bloom.database.entity.users;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import org.springframework.format.annotation.NumberFormat;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users_tb")
+@Data
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @NumberFormat
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Email
+    @Column(name = "email", nullable = false)
+    private String email;
+
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Transient
+    private String confirmPassword;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "user_active", nullable = false)
+    private boolean userActive = true;
+
+}
