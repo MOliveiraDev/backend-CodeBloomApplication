d7a5f2f52513e2fc8cbb54377371f5ad20aa67ce
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-01 18:16:37 -0300

    Feat: create PasswordIncorrectException

---
+++ src/main/java/com/code/bloom/exceptions/login/PasswordIncorretException.java
@@ -0,0 +1,8 @@
+package com.code.bloom.exceptions.login;
+
+public class PasswordIncorretException extends RuntimeException {
+
+    public PasswordIncorretException(String message) {
+        super(message);
+    }
+}
