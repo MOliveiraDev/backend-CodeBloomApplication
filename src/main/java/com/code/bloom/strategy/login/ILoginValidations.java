bf25cbd614296a683fb4c8090dbeb3454097b8cd
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-02 11:19:36 -0300

    deleted

--- src/main/java/com/code/bloom/strategy/login/ILoginValidations.java
+++
@@ -1,8 +0,0 @@
-package com.code.bloom.strategy.login;
-
-import com.code.bloom.dto.login.LoginRequest;
-
-public interface ILoginValidations {
-
-    void loginResponseValidation(LoginRequest loginRequest);
-}
