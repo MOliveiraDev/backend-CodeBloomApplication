ad7fc39a13ecb80ef330ef50f2870e07c6acbf59
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-04 20:48:44 -0300

    delete: remove unused classes and interfaces related to logout validation and roles

--- src/main/java/com/code/bloom/database/entity/user/Role.java
+++
@@ -1,6 +0,0 @@
-package com.code.bloom.database.entity.user;
-
-public enum Role {
-    ALUNO,
-    PROFESSOR
-}
--- src/main/java/com/code/bloom/strategy/logout/ILogoutValidations.java
+++
@@ -1,7 +0,0 @@
-package com.code.bloom.strategy.logout;
-
-
-public interface ILogoutValidations {
-
-    void logoutResponseValidation(String token);
-}
