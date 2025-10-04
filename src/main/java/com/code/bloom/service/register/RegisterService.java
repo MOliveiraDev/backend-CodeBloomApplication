0bd66e13e7a9fb903dec63c1af55ef653b470c79
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-04 20:51:41 -0300

    feat: update SecurityConfig to add professor role and new logout endpoint

--- src/main/java/com/code/bloom/service/register/RegisterService.java
+++ src/main/java/com/code/bloom/service/register/RegisterService.java
@@ -1,16 +1,21 @@
 package com.code.bloom.service.register;
 
-import com.code.bloom.database.entity.user.Role;
-import com.code.bloom.database.entity.user.UserEntity;
+import com.code.bloom.database.entity.user.*;
+import com.code.bloom.database.repository.user.RoleRepository;
+import com.code.bloom.database.repository.user.TokenRepository;
 import com.code.bloom.database.repository.user.UserRepository;
+import com.code.bloom.database.repository.user.UserRoleRepository;
 import com.code.bloom.dto.register.RegisterRequest;
 import com.code.bloom.dto.register.RegisterResponse;
+import com.code.bloom.service.jwt.JwtService;
+import com.code.bloom.service.jwt.TokenService;
 import com.code.bloom.strategy.register.IRegisterValidations;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.*;
 
 @Service
@@ -18,40 +23,83 @@ import java.util.*;
 public class RegisterService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final UserRoleRepository userRoleRepository;
     private final PasswordEncoder passwordEncoder;
     private final List<IRegisterValidations> registerValidationsList;
+    private final JwtService jwtService;
+    private final TokenService tokenService;
+
 
     @Transactional
     public RegisterResponse newUserAluno(RegisterRequest request) {
+        registerValidationsList.forEach(registerValidations -> registerValidations.registerResponseValidations(request));
 
-        registerValidationsList.forEach(strategy -> strategy.registerResponseValidations(request));
-
-        UserEntity user = new UserEntity();
-        user.setUsername(request.username());
-        user.setPhoneNumber(request.phoneNumber());
-        user.setEmail(request.email());
-        user.setPassword(passwordEncoder.encode(request.password()));
-        user.setRole(Role.ALUNO);
+        UserEntity user = buildBaseUser(request);
         userRepository.save(user);
 
-        return new RegisterResponse("Usuário cadastrado com sucesso");
+        RoleEntity alunoRole = roleRepository.findByRoleName("ALUNO")
+                .orElseGet(() -> {
+                    RoleEntity newRole = new RoleEntity();
+                    newRole.setRoleName("ALUNO");
+                    return roleRepository.save(newRole);
+                });
+
+
+        linkUserToRole(user, alunoRole);
+        return new RegisterResponse("Usuário aluno registrado com sucesso");
+
 
     }
 
+    @Transactional
     public RegisterResponse newUserProfessor(RegisterRequest request) {
+        registerValidationsList.forEach(registerValidations -> registerValidations.registerResponseValidations(request));
 
-        registerValidationsList.forEach(strategy -> strategy.registerResponseValidations(request));
-
-        UserEntity user = new UserEntity();
-
-        user.setUsername(request.username());
-        user.setPhoneNumber(request.phoneNumber());
-        user.setEmail(request.email());
-        user.setPassword(passwordEncoder.encode(request.password()));
-        user.setRole(Role.PROFESSOR);
+        UserEntity user = buildBaseUser(request);
         userRepository.save(user);
 
-        return new RegisterResponse("Professor cadastrado com sucesso");
+        RoleEntity professorRole = roleRepository.findByRoleName("PROFESSOR")
+                .orElseGet(() -> {
+                    RoleEntity newRole = new RoleEntity();
+                    newRole.setRoleName("PROFESSOR");
+                    return roleRepository.save(newRole);
+                });
 
+        linkUserToRole(user, professorRole);
+
+
+        return new RegisterResponse("Usuário professor registrado com sucesso");
     }
+
+    private UserEntity buildBaseUser(RegisterRequest request) {
+        UserEntity user = new UserEntity();
+        user.setUsername(request.username());
+        user.setPhoneNumber(request.phoneNumber());
+        user.setBirthdayDate(request.birthdayDate());
+        user.setEmail(request.email());
+        user.setPassword(passwordEncoder.encode(request.password()));
+        user.setCreatedAt(new java.sql.Timestamp(System.currentTimeMillis()));
+        user.setAge(calculateAge(request.birthdayDate()));
+        user.setUserStatus(UserStatus.OFFLINE);
+        return user;
+    }
+
+    private int calculateAge(Date birthDate) {
+        if (birthDate == null) return 0;
+
+        java.time.LocalDate birthLocalDate = new java.sql.Date(birthDate.getTime()).toLocalDate();
+        java.time.LocalDate today = java.time.LocalDate.now();
+
+        return java.time.Period.between(birthLocalDate, today).getYears();
+    }
+
+
+    private void linkUserToRole(UserEntity user, RoleEntity role) {
+        UserRoleEntity userRole = new UserRoleEntity();
+        userRole.setUser(user);
+        userRole.setRole(role);
+        userRoleRepository.save(userRole);
+    }
+
 }
