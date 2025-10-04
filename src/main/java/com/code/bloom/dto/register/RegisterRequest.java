4af6ab0b1d308f9330894d4ac451fc51d0cc1185
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-04 20:49:47 -0300

    feat: add birthdayDate field to RegisterRequest

--- src/main/java/com/code/bloom/dto/register/RegisterRequest.java
+++ src/main/java/com/code/bloom/dto/register/RegisterRequest.java
@@ -1,4 +1,6 @@
 package com.code.bloom.dto.register;
 
-public record RegisterRequest(String username, String phoneNumber, String email, String password, String confirmPassword) {
+import java.util.Date;
+
+public record RegisterRequest(String username, String phoneNumber, Date birthdayDate, String email, String password, String confirmPassword) {
 }
