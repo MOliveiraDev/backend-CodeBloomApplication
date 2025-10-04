736a8073991d3c45567a3349988992e6734bb41b
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-04 20:50:32 -0300

    feat: add LogoutController with endpoint for handling user logout

--- src/main/java/com/code/bloom/database/entity/user/UserEntity.java
+++ src/main/java/com/code/bloom/database/entity/user/UserEntity.java
@@ -4,13 +4,19 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.Data;
 import org.springframework.format.annotation.NumberFormat;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users_tb")
 @Data
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -28,6 +34,15 @@ public class UserEntity {
     @Column(name = "email", nullable = false)
     private String email;
 
+    @Column(name = "birthday_date", nullable = false)
+    private Date birthdayDate;
+
+    @Column(name = "age", nullable= false)
+    private Integer age;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp createdAt;
+
 
     @Column(name = "password", nullable = false)
     private String password;
@@ -35,11 +50,40 @@ public class UserEntity {
     @Transient
     private String confirmPassword;
 
-    @Column(name = "role", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Role role;
-
     @Column(name = "user_active", nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserStatus userStatus;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<UserRoleEntity> userRole;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<TokenEntity> tokens;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return userRole.stream()
+                .map(userRole -> (GrantedAuthority) () -> "ROLE_" + userRole.getRole().getRoleName().toUpperCase())
+                .toList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
