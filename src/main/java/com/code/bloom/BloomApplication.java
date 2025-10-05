fb09354c2cf915ba94b7f922000694a1a77e7ebf
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-05 15:01:23 -0300

    feat: enable Scheduling for application

--- src/main/java/com/code/bloom/BloomApplication.java
+++ src/main/java/com/code/bloom/BloomApplication.java
@@ -3,7 +3,9 @@ package com.code.bloom;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class BloomApplication {
 
