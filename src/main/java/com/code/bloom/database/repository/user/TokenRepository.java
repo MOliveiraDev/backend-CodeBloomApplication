a3827cb244e9d237a814ad4388395739ffe795a1
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-05 16:36:34 -0300

    refactor: remove useless import

--- src/main/java/com/code/bloom/database/repository/user/TokenRepository.java
+++ src/main/java/com/code/bloom/database/repository/user/TokenRepository.java
@@ -1,13 +1,8 @@
 package com.code.bloom.database.repository.user;
 
 import com.code.bloom.database.entity.user.TokenEntity;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
