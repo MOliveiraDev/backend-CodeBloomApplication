919d38635fc8e6b9e47fd23a9ab1a01bfb06cff0
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-05 15:03:09 -0300

    refactor: remove List findAllByUserAndIsLoggedOutFalse for LoggedOutTrue List

--- src/main/java/com/code/bloom/database/repository/user/TokenRepository.java
+++ src/main/java/com/code/bloom/database/repository/user/TokenRepository.java
@@ -1,12 +1,13 @@
 package com.code.bloom.database.repository.user;
 
 import com.code.bloom.database.entity.user.TokenEntity;
-import com.code.bloom.database.entity.user.UserEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,9 +15,5 @@ public interface TokenRepository extends JpaRepository<TokenEntity, Long> {
 
     Optional<TokenEntity> findByToken(String token);
 
-    List<TokenEntity> findAllByUserAndIsLoggedOutFalse(UserEntity user);
-
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
-    @Query("update TokenEntity t set t.isLoggedOut = true where t.user = :user and t.isLoggedOut = false")
-    int logoutAllByUser(@Param("user") UserEntity user);
+    List<TokenEntity> findAllByIsLoggedOutTrue();
 }
