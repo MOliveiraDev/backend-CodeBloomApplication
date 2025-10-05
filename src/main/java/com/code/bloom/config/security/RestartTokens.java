38a550ca45d77c6807d89370a28ef2d7171c41f3
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-05 15:05:20 -0300

    feat: add scheduled for three minutes to clear all logged out tokens

---
+++ src/main/java/com/code/bloom/config/security/RestartTokens.java
@@ -0,0 +1,18 @@
+package com.code.bloom.config.security;
+
+import com.code.bloom.service.jwt.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RestartTokens {
+
+    private final TokenService tokenService;
+
+    @Scheduled(fixedRate = 180000)
+    public void cleanLoggedOutTokens() {
+        tokenService.deleteLoggedOutTokens();
+    }
+}
