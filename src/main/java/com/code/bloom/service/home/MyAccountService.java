072f764c2d7db5896c9b16a4b4d9973304920eea
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-18 17:12:47 -0300

    refactor: replace direct authentication logic with IAuthenticationStrategy in MyAccountService

--- src/main/java/com/code/bloom/service/home/MyAccountService.java
+++ src/main/java/com/code/bloom/service/home/MyAccountService.java
@@ -1,35 +1,25 @@
 package com.code.bloom.service.home;
 
+
 import com.code.bloom.database.entity.user.UserEntity;
 import com.code.bloom.dto.account.MyAccountResponse;
 import com.code.bloom.dto.account.UserRoleResponse;
-import com.code.bloom.database.repository.user.UserRepository;
+import com.code.bloom.strategy.auth.IAuthenticationStrategy;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.AccessDeniedException;
+
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MyAccountService {
 
-    private final UserRepository userRepository;
+    private final IAuthenticationStrategy iAuthenticationStrategy;
 
-    public MyAccountResponse getMyProfile() throws AccessDeniedException {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    public MyAccountResponse getMyProfile() {
 
-        if (auth == null || !auth.isAuthenticated() || "anonymousUser".equals(auth.getPrincipal())) {
-            throw new AccessDeniedException("Acesso negado");
-        }
-
-        String username = auth.getName();
-
-        UserEntity user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+        UserEntity user = iAuthenticationStrategy.getAuthenticatedUser();
 
         List<UserRoleResponse> roles = user.getUserRole().stream()
                 .map(r -> new UserRoleResponse(r.getRole().getRoleName()))
