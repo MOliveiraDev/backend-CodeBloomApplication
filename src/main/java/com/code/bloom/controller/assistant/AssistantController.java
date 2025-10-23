39a3a98cd2a1ae575ffd15a0c21bca2816a25a95
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-23 08:31:12 -0300

    refactor: add new request for get historic messages

--- src/main/java/com/code/bloom/controller/assistant/AssistantController.java
+++ src/main/java/com/code/bloom/controller/assistant/AssistantController.java
@@ -1,14 +1,16 @@
 package com.code.bloom.controller.assistant;
 
+import com.code.bloom.dto.account.ChatsHistoricResponse;
+import com.code.bloom.dto.account.ChatsMessagesResponse;
 import com.code.bloom.dto.assistant.MessageRequest;
 import com.code.bloom.dto.assistant.MessageResponse;
 import com.code.bloom.service.assistant.AssistantService;
+import com.code.bloom.service.assistant.UserHistoricChatsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.AccessDeniedException;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,10 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class AssistantController {
 
     private final AssistantService assistantService;
+    private final UserHistoricChatsService historicChatsService;
 
     @PostMapping("/chat")
     public ResponseEntity<MessageResponse> sendMessage(@RequestBody MessageRequest request) {
         MessageResponse response = assistantService.processUserMessage(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/chatbot-historic")
+    public ResponseEntity<ChatsHistoricResponse> getChatBotHistoric() throws AccessDeniedException {
+        ChatsHistoricResponse response = historicChatsService.getHistoricChats();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/chatbot-messages")
+    public ResponseEntity<ChatsMessagesResponse> getChatBotMessages() throws AccessDeniedException {
+        ChatsMessagesResponse response = historicChatsService.getChatMessages();
         return ResponseEntity.ok(response);
     }
 }
