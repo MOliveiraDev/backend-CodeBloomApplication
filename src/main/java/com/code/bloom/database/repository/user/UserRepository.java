eeab9ac4f7fbf24cdc04bcc64f1e6172b5cdfcae
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-25 15:11:18 -0300

    refactor: removed useless imports

--- src/main/java/com/code/bloom/database/repository/user/UserRepository.java
+++ src/main/java/com/code/bloom/database/repository/user/UserRepository.java
@@ -2,8 +2,6 @@ package com.code.bloom.database.repository.user;
 
 import com.code.bloom.database.entity.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
