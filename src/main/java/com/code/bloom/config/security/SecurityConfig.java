ba502530c44e2abdf86c3fb392bc3494cce2e622
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-10-29 22:46:56 -0300

    refactor: enable cors

--- src/main/java/com/code/bloom/config/security/SecurityConfig.java
+++ src/main/java/com/code/bloom/config/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors().and()
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
