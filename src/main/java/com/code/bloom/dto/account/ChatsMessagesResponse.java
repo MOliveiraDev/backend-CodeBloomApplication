0e7b2400a02dd33292e92cc98b0360a079318b49
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-11-06 10:31:16 -0300

    refactor: remove timeStamp

--- src/main/java/com/code/bloom/dto/account/ChatsMessagesResponse.java
+++ src/main/java/com/code/bloom/dto/account/ChatsMessagesResponse.java
@@ -2,9 +2,8 @@ package com.code.bloom.dto.account;
 
 import com.code.bloom.dto.assistant.MessageDto;
 
-import java.sql.Timestamp;
 import java.util.List;
 
-public record ChatsMessagesResponse(String title, Timestamp createdAt, List<MessageDto> messages) {
+public record ChatsMessagesResponse(String title, List<MessageDto> messages) {
 }
 
